f657e0a7e5b6885d71b32edb885c99fa781139cf
Gamal Samadov <gamalsamadov@gmail.com>
2024-01-07 13:24:19 +0300

    Mobile responsive 📱Bottombar added ✅

--- constants/index.js
+++ constants/index.js
@@ -16,8 +16,8 @@ export const sidebarLinks = [
 	},
 	{
 	  imgURL: "/assets/create.svg",
-	  route: "/create-thread",
-	  label: "Create Thread",
+	  route: "/new-post",
+	  label: "New Post",
 	},
 	{
 	  imgURL: "/assets/community.svg",
